e65e53e2642daeff61536c05af49a49f5cc52d39
Teddy Gesbert <teddygesbert@gmail.com>
2024-06-25 02:15:07 +0200

    Optionnal Challenge

--- helloworld.py
+++ helloworld.py
@@ -3,9 +3,15 @@ import sys
 def helloWorld(name):
     print("Hello, " + name+"!")
 
+def greetEveryone(sys):
+    for i in range(len(sys.argv)):
+        if(i!=0):
+            print("Hello, " + sys.argv[i] + "!")
+
 def main():
-    name = sys.argv[1]
-    helloWorld(name)
+    #name = sys.argv[1]
+    #helloWorld(name)
+    greetEveryone(sys)
 
 if __name__ == "__main__":
     main()
